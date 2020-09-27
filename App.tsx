2746cf4c71d887dc9eebf29ccfd74b31dbcd64c8
UdayPatel <uday.it1911@gmail.com>
2020-09-28 00:11:36 +0530

    Seprate the store from app.tsx file anf added that code into store.tsx file and import it in app.tsx

--- App.tsx
+++ App.tsx
@@ -13,10 +13,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { enableScreens } from 'react-native-screens';
 
-import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-
-import nameReducers from './store/Reducers/NameReducer';
 
 import MainNavigation from './navigation/MainNavigation';
 
