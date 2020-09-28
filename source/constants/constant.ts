196e7b3214b8e600f5e8a78288d973ed7d1f4f23
UdayPatel <uday.it1911@gmail.com>
2020-09-28 17:56:09 +0530

    Updated constant file

---
+++ source/constants/constant.ts
@@ -0,0 +1,11 @@
+import {Platform} from 'react-native';
+
+/**
+ * Its Check if device OS is Android
+ */
+export const IS_ANDROID = Platform.OS === 'android';
+
+/**
+ * Its Check if device OS is iOS
+ */
+export const IS_IOS = Platform.OS === 'ios';
